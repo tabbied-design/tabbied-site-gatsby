2a176f3f5c01fe7a9cb59dc081c4ca013fc5d919
Ye Joo Park <subwaymatch@gmail.com>
2019-11-13 19:58:57 +0900

    Work-in-progress customize grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,16 +10,18 @@ const HeroSection = () => {
     <div id="section-hero">
       <div className="container">
         <div className="row">
-          <div className="col-md-12 hero-background">
-            <p className="hero-text">
-              Customizable,
-              <br /> CSS generated artwork
-              <br /> for designers.
-            </p>
+          <div className="hero-background">
+            <div className="col-md-6">
+              <p className="hero-text">
+                Customizable,
+                <br /> CSS generated artwork
+                <br /> for designers.
+              </p>
 
-            <div className="hero-actions">
-              <a className="btn-action">Try for free</a>
-              <a className="btn-watch-video">Watch the video</a>
+              <div className="hero-actions">
+                <a className="btn-action">Try for free</a>
+                <a className="btn-watch-video">Watch the video</a>
+              </div>
             </div>
           </div>
         </div>
