94603eeb02569db95d17e3c3d7ff405bf2ff20ea
Ye Joo Park <subwaymatch@gmail.com>
2019-11-20 09:56:38 +0900

    Add google-fonts plugin for Nunito Sans and Material Icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-plugin-google-fonts",
+      options: {
+        fonts: ["material icons", "nunito sans:200,300,400,700"],
+      },
+    },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
