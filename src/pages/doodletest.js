d2f22fdf2c18cd8774b5c6d2b4f0394bf9cf8a69
Ye Joo Park <subwaymatch@gmail.com>
2019-11-29 03:53:19 +0700

    Work-in-progress merge style into css-doodle tag

--- src/pages/doodletest.js
+++ src/pages/doodletest.js
@@ -1,73 +1,89 @@
 import React from "react"
 import "css-doodle"
 
-const doodleStyle = `
-css-doodle#demo-doodle {
-  /* Default colors */
-  --color0:#FFFFFF;
-  --color1:#FFA1C6;
-  --color2:#3FFFB2;
-  --color3:#3EECFF;
-  --color4:#1B4075;
-  --color5:#326DC9;
+class DoodleTest extends React.Component {
+  constructor(props) {
+    super(props)
 
-  /* Random color generator variable */
-  --randomColor: @p(var(--color1), var(--color2), var(--color3), var(--color4), var(--color5));
-  --rule: (
-    /*Frequency options of 0.2, 0.4, 0.6, 0.8, 1.0 */	
-    @random (0.8) { 
-      background-color: var(--randomColor);
-      -webkit-clip-path: polygon(@pick(
-        '0 0, 101% 0, 101% 101%',
-        '0 0, 101% 0, 0 101%',
-        '0 0, 101% 101%, 0 101%',
-        '101% 0, 101% 101%, 0 101%'
-      ));
-      clip-path: polygon(@pick(
-        '0 0, 101% 0, 101% 101%',
-        '0 0, 101% 0, 0 101%',
-        '0 0, 101% 101%, 0 101%',
-        '101% 0, 101% 101%, 0 101%'
-      ));
-      -webkit-transition: .2s ease @rand(200ms);
-      transition: .2s ease @rand(200ms);
-      -webkit-transform: clip-path;
-      transform: clip-path;
+    this.state = {
+      grid: "6x4",
     }
 
-  );
-}
-`
+    this.doodleRef = React.createRef()
+  }
 
-class DoodleTest extends React.Component {
+  changeGrid() {
+    this.setState({
+      grid: "12x8",
+    })
+  }
+
+  redraw() {
+    this.doodleRef.current.update()
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-md-12">
-            <style dangerouslySetInnerHTML={{ __html: doodleStyle }} />
-            <css-doodle id="demo-doodle" use="var(--rule)">
+            <style>
               {`
-            :doodle {
-              @grid: 12x8 / 100%;
-              width:320px;
-              height:480px;
-              overflow:hidden;
-              -webkit-box-shadow:0 0 5px #ddd;
-              box-shadow:0 0 5px #ddd;
-              position:absolute;
-              left:25%;
-              top:0;
-              margin-left:-160px;
-              text-align:center;
-              box-sizing:border-box
-            }
-            :container {
-              background:var(--color0);
-              overflow:hidden;
-            }
-            `}
+              css-doodle#demo-doodle {
+                --color0: #ff6655;
+                --color1: #CED3D9;
+              }
+              `}
+            </style>
+            <css-doodle
+              id="demo-doodle"
+              ref={this.doodleRef}
+              grid={this.state.grid}
+            >
+              {`
+                --color0: #FFFFFF;
+                --color1: #CED3D9;
+                --color2: #FF3D8B;
+                --color3: #3FFFB2;
+                --color4: #275AA6;
+                --color5: #3EECFF;
+
+                --randomColor: @p(var(--color2), var(--color3), var(--color4), var(--color5));
+                
+                :doodle {
+                  width: 320px; 
+                  height: 480px; 
+						      box-shadow:0 0 5px #ddd;
+                }
+
+                :container {
+                }
+
+                @random(0.6) {
+                  :after {
+                    content: '';
+                    background: var(--randomColor);
+                    @size: @rand(20px);
+                    clip-path: @pick(polygon(50% 0%, 0% 100%, 100% 100%), circle(50% at 50% 50%), polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%));
+                    transform:rotate(@pick(0deg, 360deg));
+                    transition: transform ease @rand(200ms, 600ms);
+                    }
+                }
+
+                /*Frequency options of 0.2, 0.4, 0.6, 0.8, 1.0 */
+                @random(0.6) { 
+                  -webkit-box-shadow:0 -1px 0 var(--color1); 
+                  box-shadow:0 -1px 0 var(--color1); 
+                }
+
+                /*Frequency options of 0.2, 0.4, 0.6, 0.8, 1.0 */
+                @random(0.6) { 
+                  -webkit-box-shadow:-1px 0 0 var(--color1);  
+                  box-shadow:-1px 0 0 var(--color1);  
+                }
+              `}
             </css-doodle>
+            <div onClick={() => this.redraw()}>Redraw</div>
           </div>
         </div>
       </div>
